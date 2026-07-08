3874e2d663fd1b53905d4d2f59f3d37f982e0f73
Shayan Patel <shayan.patel@canonical.com>
2026-07-08 14:54:54 -0400

    Add stub just and tox targets for identity UATs

---
+++ tests/test_identity.py
@@ -0,0 +1,10 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test_stub():
+    """Stub identity UAT test"""
